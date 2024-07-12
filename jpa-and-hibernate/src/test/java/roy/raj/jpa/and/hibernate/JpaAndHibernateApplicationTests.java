cdd1a2ee2946f8d26e5f8c4681350972a11523bf
UNKNOWN <rajr86642@gmail.com>
2024-07-12 16:30:03 +0530

    jpa and hibernate files added

---
+++ jpa-and-hibernate/src/test/java/roy/raj/jpa/and/hibernate/JpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package roy.raj.jpa.and.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
