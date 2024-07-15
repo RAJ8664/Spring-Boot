6f240e618498404f720de2a52be71b1231c678b9
UNKNOWN <rajr86642@gmail.com>
2024-07-15 19:29:20 +0530

    request param

--- TODO-WEB-APP/src/main/java/com/roy/raj/TODO/WEB/APP/Login/LoginController.java
+++ TODO-WEB-APP/src/main/java/com/roy/raj/TODO/WEB/APP/Login/LoginController.java
@@ -1,12 +1,17 @@
 package com.roy.raj.TODO.WEB.APP.Login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class LoginController {
+
+   private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/simple-login")
     public String login() {
@@ -17,6 +22,7 @@ public class LoginController {
     @RequestMapping("/login-with-param")
     public String login(@RequestParam String name, ModelMap model) {
         model.put("name", name);
+        logger.debug("current_name is {}" , name);
         //mention ${name} in the login.jsp file
         return "login";
     }
