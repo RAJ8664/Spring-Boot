fae5059197c70e11710e2523aa77f5441a1d52a1
UNKNOWN <rajr86642@gmail.com>
2024-09-26 20:18:15 +0530

    ReactJs Component Creation

---
+++ npm js node react/FrontEnd/practice/practice-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        The Todo Application Updated
+        <FirstComponent></FirstComponent>
+    </div>
+  );
+}
+
+function FirstComponent() {
+  return (
+    <div className = "FirstComponent">This is My First Created Component</div>
+  ); 
+}
+
+export default App;
